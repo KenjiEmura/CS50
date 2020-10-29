ba4a6e45eb78a7b055ff864624db20c2d089297b
Kenji Emura <kenjiemura@gmail.com>
2020-10-29 20:35:23 +0900

    Developed the table of the inbox mails

--- Course-2/Project 3 - Mail/mail/static/mail/inbox.js
+++ Course-2/Project 3 - Mail/mail/static/mail/inbox.js
@@ -1,18 +1,9 @@
 document.addEventListener('DOMContentLoaded',() => {
 
+  loadIndex();
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => {
-    load_mailbox('inbox');
-    // fetch('/emails/inbox', {
-    //   method: 'GET'
-    // })
-    // .then(response => response.json())
-    // .then(result => {
-    //     // Print result
-    //     console.log(result);
-    // });
-    
-  });
+  document.querySelector('#inbox').addEventListener('click', loadIndex );
+  
   document.querySelector('#sent').addEventListener('click', () => {
     load_mailbox('sent');
     fetch('/emails/sent', {
@@ -75,3 +66,43 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
 
+function loadIndex() {
+  load_mailbox('inbox');
+  fetch('/emails/inbox', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      let table_headers = [
+        "<th class='sender-col'>Sender</th>",
+        "<th class='subject-col'>Subject</th>",
+        "<th class='timestamp-col'>Date</th>"
+      ];
+      document.querySelector('#emails-view').insertAdjacentHTML('beforeend', `<table id="emails-table"><thead>${table_headers.join('')}</thead><tbody></tbody></table>`);
+      result.forEach(newRow);
+  });
+}
+
+function newRow(rowInfo) {
+  // Create a new row in the table
+  const row = document.querySelector('tbody').insertRow();
+
+  console.log(rowInfo);
+
+  row.className = 'mail-row';
+  let sender = row.insertCell();
+  sender.className = 'sender-col';
+  sender.innerHTML = rowInfo.sender;
+
+  let subject = row.insertCell();
+  subject.className = 'subject-col';
+  subject.innerHTML = rowInfo.subject;
+
+  let timestamp = row.insertCell();
+  timestamp.className = 'timestamp-col';
+  timestamp.innerHTML = rowInfo.timestamp;
+
+
+}
