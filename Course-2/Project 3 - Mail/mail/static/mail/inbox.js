07d95bc858187000877474ebf26de8a3c281419e
Kenji Emura <kenjiemura@gmail.com>
2020-10-30 06:05:19 +0900

    Fixed a bug in the "compose-form"  behaviour

--- Course-2/Project 3 - Mail/mail/static/mail/inbox.js
+++ Course-2/Project 3 - Mail/mail/static/mail/inbox.js
@@ -4,8 +4,34 @@ document.addEventListener('DOMContentLoaded',() => {
   document.querySelector('#inbox').addEventListener('click', loadInbox );
   document.querySelector('#sent').addEventListener('click', loadSent );
 
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#archived').addEventListener('click', loadArchived );
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+
+  // document.querySelector('#compose-form').addEventListener('submit', () => {
+
+  //   const form_recipients = document.querySelector('#compose-recipients').value;
+  //   const form_subject = document.querySelector('#compose-subject').value;
+  //   const form_body = document.querySelector('#compose-body').value;
+
+  //   fetch('/emails', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //         recipients: form_recipients,
+  //         subject: form_subject,
+  //         body: form_body
+  //     })
+  //   })
+  //   .then(response => response.json())
+  //   .then(result => {
+  //       // Print result
+  //       console.log(result);
+  //   });
+
+  //   loadSent();
+
+  //   return false;
+  // });
 
   document.querySelector('#compose-form').onsubmit = () => {
     const form_recipients = document.querySelector('#compose-recipients').value;
@@ -20,18 +46,15 @@ document.addEventListener('DOMContentLoaded',() => {
           body: form_body
       })
     })
-    .then(response => response.json())
-    .then(result => {
-        // Print result
-        console.log(result);
-    });
+    .then(loadSent)
 
-    loadSent();
     return false;
   }
+
   // By default, load the inbox
   loadInbox();
 });
+
 
 
 
@@ -48,7 +71,7 @@ function compose_email() {
 
 
 
-function load_mailbox(mailbox) {
+function load_mailbox() {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -56,9 +79,9 @@ function load_mailbox(mailbox) {
 
 
 
-function loadInbox() {
 
-  load_mailbox('inbox');
+function loadInbox() {
+  load_mailbox();
 
   // Get the data from the API
   fetch('/emails/inbox', {
@@ -74,7 +97,7 @@ function loadInbox() {
 
     // Show the mailbox name and create the table inside the <div id="emails-view">
     document.querySelector('#emails-view').innerHTML = "<h3>Inbox</h3>";
-    document.querySelector('#emails-view').insertAdjacentHTML('beforeend', `<table id="emails-table"><thead>${table_headers.join('')}</thead><tbody></tbody></table>`);
+    document.querySelector('#emails-view').insertAdjacentHTML('beforeend', `<table id="inbox-table"><thead>${table_headers.join('')}</thead><tbody></tbody></table>`);
     
     // Populate the table
     result.forEach( rowInfo => {
@@ -100,9 +123,11 @@ function loadInbox() {
   });
 }
 
-function loadSent() {
 
-  load_mailbox('sent');
+
+
+function loadSent() {
+  load_mailbox();
 
   // Get the data from the API
   fetch('/emails/sent', {
@@ -118,9 +143,7 @@ function loadSent() {
 
     // Show the mailbox name and create the table inside the <div id="emails-view">
     document.querySelector('#emails-view').innerHTML = "<h3>Sent</h3>";
-    document.querySelector('#emails-view').insertAdjacentHTML('beforeend', `<table id="emails-table"><thead>${table_headers.join('')}</thead><tbody></tbody></table>`);
-
-    console.log(result);
+    document.querySelector('#emails-view').insertAdjacentHTML('beforeend', `<table id="sent-table"><thead>${table_headers.join('')}</thead><tbody></tbody></table>`);
 
     // Populate the table
     result.forEach( rowInfo => {
@@ -131,7 +154,56 @@ function loadSent() {
       // Sent-To Column
       let sent_to = row.insertCell();
       sent_to.className = 'sender-col';
-      sent_to.innerHTML = rowInfo.recipients.join();
+      sent_to.innerHTML = rowInfo.recipients.join(', ');
+
+      // Subject Column
+      let subject = row.insertCell();
+      subject.className = 'subject-col';
+      subject.innerHTML = rowInfo.subject;
+
+      // Timestamp Column
+      let timestamp = row.insertCell();
+      timestamp.className = 'timestamp-col';
+      timestamp.innerHTML = rowInfo.timestamp;
+    });
+  });
+}
+
+
+
+
+function loadArchived() {
+  load_mailbox();
+
+  // Get the data from the API
+  fetch('/emails/archive', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(result => {
+
+    console.log(result);
+
+    let table_headers = [
+      "<th class='sender-col'>Sender</th>",
+      "<th class='subject-col'>Subject</th>",
+      "<th class='timestamp-col'>Date</th>"
+    ];
+
+    // Show the mailbox name and create the table inside the <div id="emails-view">
+    document.querySelector('#emails-view').innerHTML = "<h3>Archived</h3>";
+    document.querySelector('#emails-view').insertAdjacentHTML('beforeend', `<table id="archived-table"><thead>${table_headers.join('')}</thead><tbody></tbody></table>`);
+
+    // Populate the table
+    result.forEach( rowInfo => {
+      // Create a new row in the table
+      const row = document.querySelector('tbody').insertRow();
+      row.className = 'mail-row';
+
+      // Sender Column
+      let sender = row.insertCell();
+      sender.className = 'sender-col';
+      sender.innerHTML = rowInfo.sender;
 
       // Subject Column
       let subject = row.insertCell();
