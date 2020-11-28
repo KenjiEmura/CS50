6b7f944c1259858af84ca79234053b995288d8b4
Kenji Emura <kenjiemura@gmail.com>
2020-11-29 02:52:20 +0900

    Optimized the "SVG" handling for the "Like Button"

--- Course-2/Project 4 - Network/network/static/network/likes.js
+++ Course-2/Project 4 - Network/network/static/network/likes.js
@@ -6,15 +6,13 @@ document.addEventListener('DOMContentLoaded',() => {
     const likes = document.querySelectorAll('.like svg, .unlike svg').forEach( like => {
         like.addEventListener("click", () => {
             let id = like.parentNode.parentNode.querySelector('input').value;
-            console.log(id);
             fetch('like', {
                 method: 'PUT',
                 headers: {'X-CSRFToken': csrftoken},
                 body: JSON.stringify({
                     id: id,
                 })
-            }).then(response => response.json())
-            .then(result => {
+            }).then(result => {
                 let likeDiv = like.parentNode.parentNode.querySelector('.like')
                 let unlikeDiv = like.parentNode.parentNode.querySelector('.unlike')
                 let likeCount = like.parentNode.parentNode.querySelector('#like-count')
