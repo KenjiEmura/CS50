c86a8e95897d6bcdff3005bd4eeede2237ba7d09
Kenji Emura <kenjiemura@gmail.com>
2020-12-03 23:14:20 +0900

    Implemented hide/show edit form on posts

--- Course-2/Project 4 - Network/network/static/network/index.js
+++ Course-2/Project 4 - Network/network/static/network/index.js
@@ -34,4 +34,42 @@ document.addEventListener('DOMContentLoaded',() => {
         });
     }
 
+
+    document.querySelectorAll('.post-container .edit').forEach( edit => {
+        let post_id = edit.parentNode.querySelector('input').value;
+        let form_container = edit.parentNode.querySelector('form');
+        let post_body = edit.parentNode.querySelector('p.post-body');
+        let text_area = form_container.querySelector('textarea').value = post_body.innerHTML;
+        edit.addEventListener('click', () => {
+            if ( form_container.style.display == 'none' ) {
+                form_container.style.display = 'block';
+                post_body.style.display = 'none';
+                edit.innerHTML = 'Close';
+            } else {
+                form_container.style.display = 'none';
+                post_body.style.display = 'block';
+                edit.innerHTML = 'Edit';
+            }
+        });
+
+    });
+    
+    
+    // onsubmit = () => {
+    //     const form_recipients = document.querySelector('#compose-recipients').value;
+    //     const form_subject = document.querySelector('#compose-subject').value;
+    //     const form_body = document.querySelector('#compose-body').value;
+
+    //     fetch('/emails', {
+    //         method: 'POST',
+    //         body: JSON.stringify({
+    //             recipients: form_recipients,
+    //             subject: form_subject,
+    //             body: form_body
+    //         })
+    //     })
+    //     .then(loadSent);
+    //     return false;
+    // }
+
 })
