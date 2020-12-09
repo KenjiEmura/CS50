855b381647ab3d99bccfe5995684fbf4d19dd3cd
Kenji Emura <kenjiemura@gmail.com>
2020-12-09 13:30:56 +0900

    Improved the pagination interface

--- Course-2/Project 4 - Network/network/static/network/index.js
+++ Course-2/Project 4 - Network/network/static/network/index.js
@@ -94,5 +94,52 @@ document.addEventListener('DOMContentLoaded',() => {
     });
     
 
+    const page = document.querySelector('input#page').value;
+    const total_pages = document.querySelector('input#total_pages').value;
+
+    function create_pagination(container) {
+
+        // Create the pagination <ul>
+        let ul = document.createElement('ul');
+        ul.setAttribute("class", 'pagination');
+
+        container.insertAdjacentElement('beforeend', ul);
+
+        for (let step = 1; step <= total_pages; step++) {
+
+            // Create the <li> element
+            let li = document.createElement('li');
+            li.className = "page-item";
+
+            // Check if the current step matches the current page
+            if (step == page) {
+                li.className = 'page-item active';
+            } else {
+                li.className = 'page-item';
+            }
+            
+            // Create the <a> element that goes inside the <li> and add the page number
+            let a = document.createElement('a');
+            a.setAttribute('href', '?page=' + step);
+            a.innerHTML = step;
+            a.className = 'page-link';
+
+            // Insert the <a> element inside the <li>
+            li.insertAdjacentElement('beforeend', a);
+
+            // Insert the <li> element into the <ul>
+            ul.insertAdjacentElement('beforeend', li);
+        }
+
+    }
+
+    // Select the <div> in which the pagination is going to be inserted
+    let pagination_container_top = document.querySelector('div.pagination-container.top');
+    create_pagination(pagination_container_top);
+
+    let pagination_container_bottom = document.querySelector('div.pagination-container.bottom');
+    create_pagination(pagination_container_bottom);
+
+
 
 })
