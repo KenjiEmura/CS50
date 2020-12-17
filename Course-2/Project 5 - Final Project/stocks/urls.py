72d816813a98710df17489c7fc58dd75c42a6462
Kenji Emura <kenjiemura@gmail.com>
2020-12-18 00:23:06 +0900

    Implemented Login and Register functionality

--- Course-2/Project 5 - Final Project/stocks/urls.py
+++ Course-2/Project 5 - Final Project/stocks/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     # Regular URLs
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 
+    
     # API Routes
 
 ]
--- Course-2/Project 5 - Final Project/stocks/views.py
+++ Course-2/Project 5 - Final Project/stocks/views.py
@@ -36,3 +36,37 @@ def login_view(request):
             })
     else:
         return render(request, "stocks/login.html")
+
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("stocks:index"))
+
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "stocks/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "stocks/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("stocks:index"))
+    else:
+        return render(request, "stocks/register.html")
