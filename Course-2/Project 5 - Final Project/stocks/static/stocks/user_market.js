7b1a2798dd8d805834b083cdd3a6adea79ea35db
Kenji Emura <kenjiemura@gmail.com>
2021-02-14 23:14:26 +0900

    Added functionality to the User's Market Button

--- Course-2/Project 5 - Final Project/stocks/static/stocks/user_market.js
+++ Course-2/Project 5 - Final Project/stocks/static/stocks/user_market.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// Django Authentification Token
-    const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]");
+  // Django Authentification Token
+  const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]");
 
-    console.log('The JS file was loaded successfully')
+  console.log("The JS file was loaded successfully");
+
+  document.querySelectorAll(".accordion__button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const accordionContent = button.nextElementSibling;
+      const wholeAccordion = button.parentElement;
+      wholeAccordion.classList.toggle("accordion--active");
+      console.log(wholeAccordion);
+      button.classList.toggle("accordion__button--active");
+      if (button.classList.contains("accordion__button--active")) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      } else {
+        accordionContent.style.maxHeight = 0;
+      }
+    });
+  });
 });
