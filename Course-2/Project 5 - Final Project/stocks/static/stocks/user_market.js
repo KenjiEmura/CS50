0bd66a4bb243749b50b30beddcfeb45c2fcd5e16
Kenji Emura <kenjiemura@gmail.com>
2021-02-15 00:26:01 +0900

    Data got prepared to be fetched

--- Course-2/Project 5 - Final Project/stocks/static/stocks/user_market.js
+++ Course-2/Project 5 - Final Project/stocks/static/stocks/user_market.js
@@ -2,14 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Django Authentification Token
   const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]");
 
-  console.log("The JS file was loaded successfully");
-
   document.querySelectorAll(".accordion__button").forEach((button) => {
     button.addEventListener("click", () => {
       const accordionContent = button.nextElementSibling;
       const wholeAccordion = button.parentElement;
       wholeAccordion.classList.toggle("accordion--active");
-      console.log(wholeAccordion);
       button.classList.toggle("accordion__button--active");
       if (button.classList.contains("accordion__button--active")) {
         accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
@@ -17,5 +14,24 @@ document.addEventListener("DOMContentLoaded", () => {
         accordionContent.style.maxHeight = 0;
       }
     });
+  });
+
+  document.querySelectorAll("tr.stock-info").forEach((row) => {
+    const stock_id = row.querySelector("input.stock-id").value;
+    const seller_id = row.querySelector("input.seller-id").value;
+    const stock_price = row.querySelector(".price").innerHTML;
+    const stock_qty = row.querySelector(".purchase .purchase-qty");
+    const purchase_button = row.querySelector("#purchase");
+
+    purchase_button.addEventListener("click", () => {
+      console.log("Stock Price: ", parseFloat(stock_price));
+      console.log("Stock Qty: ", parseInt(stock_qty.value));
+      console.log("Total: ", parseFloat(stock_price) * parseInt(stock_qty.value));
+
+    });
+
+    // row.addEventListener("click", () => {
+    //   console.log("Stock Id: " + stock_id + ", seller id: " + seller_id);
+    // });
   });
 });
