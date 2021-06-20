c68d89d0de8daae2708ac01a466772f7552d35c5
Kenji Emura <kenjiemura@gmail.com>
2021-06-20 22:54:46 +0900

    Applied prettier to the stocks URLs file

--- Course-2/Project 5 - Final Project/stocks/urls.py
+++ Course-2/Project 5 - Final Project/stocks/urls.py
@@ -12,10 +12,11 @@ urlpatterns = [
     path("test", views.test, name="test"),
     path("user_market", views.user_market, name="user_market"),
 
-    
+
     # API Routes
-    path("API/set_sell_stock_price", views.set_sell_stock_price, name="set_stock_price"),
+    path("API/set_sell_stock_price",
+         views.set_sell_stock_price, name="set_stock_price"),
     path("API/update_for_sale", views.update_for_sale, name="update_for_sale"),
-	  path("API/trade-stock", views.trade_stock, name="trade_stock" )
+    path("API/trade-stock", views.trade_stock, name="trade_stock")
 
 ]
